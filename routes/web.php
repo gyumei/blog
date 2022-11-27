47583c8fd04024feb9b3fd3945595b9802a31c9d
gyumei <hannibal221b219@gmail.com>
2022-11-27 20:45:48 +0900

    3回目のコミットコメント

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;      //追加
+use Illuminate\Support\Facades\Route;    //追加
 /*
 |--------------------------------------------------------------------------
 | Web Routes
