5c34710bd4658a6698ba3090e65f32e590e814ac
gyumei <hannibal221b219@gmail.com>
2022-11-27 23:18:44 +0900

    適切なコミット

--- routes/web.php
+++ routes/web.php
@@ -11,12 +11,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [PostController::class, 'index']);
 
-Route::get('/posts/{post}', [PostController::class ,'show']);
 
-
-Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'
+show']);
 
 
