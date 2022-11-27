f657d0c4e3bf9db9266919f6227eb2fb9033eaf6
gyumei <hannibal221b219@gmail.com>
2022-11-27 18:53:06 +0900

    ブランチ４大二段のコミット

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,23 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-//use宣言は外部にあるクラスをPostController内にインポートできる。
-//この場合、App\Models内のPostクラスをインポートしている。
 use App\Models\Post;
-/**
- * Post一覧を表示する
- * 
- * @param Post Postモデル
- * @return array Postモデルリスト
- */
+use App\Http\Requests\PostRequest; // useする
 
 class PostController extends Controller
 {
-    public function index(Post $post)//インポートしたPostをインスタンス化して$postとして使用。
-{
-    return $post->get();//$postの中身を戻り値にする。
-}
+    public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
 
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+
+    public function create()
+    {
+        return view('posts/create');
+    }
+
+    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
 }
