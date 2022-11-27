5dae04f9bb713f1ec3bf61eb516fe5227a4b371a
gyumei <hannibal221b219@gmail.com>
2022-11-27 23:12:15 +0900

    コミットします

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,3 @@ Route::get('/posts', [PostController::class, 'index']);
 
 Route::get('/posts/create', [PostController::class, 'create']);
 
-
-
-
-Route::get('/posts/{post}', [PostController::class ,'show']);
