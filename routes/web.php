6423f42c9a573db99ec4ef0d3dbbb1ac02242211
gyumei <hannibal221b219@gmail.com>
2022-11-27 21:59:31 +0900

    ルートのコミットコメント

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,4 @@
 <?php
 <<<<<<< dev_basis01
-=======
 
-
-
-Route::get('/posts', [PostController::class, 'index']);
-
-Route::get('/posts/create', [PostController::class, 'create']);
 >>>>>>> master
